52b031c799e4ccd82e6174799c516120afc70e13
Sơn Nguyễn <son@sonng.dev>
2023-01-09 20:11:56 +0000

    feat: Set up barebone functionalities

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const {
+    browserSupportsSpeechRecognition,
+    isMicrophoneAvailable,
+    transcript,
+    listening,
+  } = useSpeechRecognition();
+
+  const recognizeSpeech = () => {
+    SpeechRecognition.startListening();
+  };
+
+  if (!browserSupportsSpeechRecognition) {
+    return (
+      <div>This browser doesn't support speech recognition. Please use Chrome.</div>
+    )
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      {!isMicrophoneAvailable && (
+        <div>Please allow microphone permission for this app to work</div>
+      )}
+
+      <button type="button" onClick={recognizeSpeech}>Talk</button>
+
+      {listening && (
+        <div>Listening...</div>
+      )}
+
+      <div>Transcript: {transcript}</div>
     </div>
-  )
+  );
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,7 @@
+// Keep this import at the top of this file
+// for 'react-speech-recognition' to work.
+import 'regenerator-runtime/runtime';
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
